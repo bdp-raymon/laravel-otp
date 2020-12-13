66368243118f34b305ef97ad74ab75935f8e8be8
Ali Shabani <alishabani9270@gmail.com>
2020-12-13 13:22:10 +0330

    document: added methods to facade document

--- src/Facades/Otp.php
+++ src/Facades/Otp.php
@@ -4,6 +4,12 @@ namespace Alish\LaravelOtp\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string issue(string $key)
+ * @method static bool revoke(string $key)
+ * @method static bool check(string $key, string $token)
+ * @method static bool use(string $key, string $token)
+ */
 class Otp extends Facade
 {
     /**
